b421f86eae964c7ca9455417c52e02e5655c97d7
alaaibr25 <94000702+alaaibr25@users.noreply.github.com>
2025-08-05 05:11:41 +0300

    Update main_movie.py
    update the update function, Add delete function.

--- main_movie.py
+++ main_movie.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 from flask_bootstrap import Bootstrap5
+from wtforms import Form, IntegerField, StringField, validators, SubmitField
+from flask_wtf import FlaskForm
+
 
 app = Flask(__name__)
 app.secret_key = ''
@@ -14,7 +17,7 @@ class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///exs.db"
 db.init_app(app)
 
 
@@ -43,15 +46,50 @@ with app.app_context():
 #     db.session.add(movie)
 #     db.session.commit()
 #-----------------------------------------------------------------------#
+#-----------------------------------------------------------------------#
+#🌶🌶🌶🌶Flask Form🌶🌶🌶🌶
+
+class MovieForm(FlaskForm):
+    rate = IntegerField('New Rate')
+    review = StringField('Review')
+    submit = SubmitField('Submit')
+
+
+
+
+#-----------------------------------------------------------------------#
+#-----------------------------------------------------------------------#
 
 @app.route('/')
 def home():
     movies = db.session.execute(db.select(Movie).order_by(Movie.title)).scalars().all()
     return render_template('index.html', movies=movies)
 
+@app.route('/update', methods=['POST', 'GET'])
+def update_page():
+    movie_form = MovieForm()
+    movie_id = request.args.get('id')
+    movie_to_update = db.get_or_404(Movie, movie_id)
+
+    if movie_form.validate_on_submit():
+
+        movie_to_update.rate = movie_form.rate.data
+        movie_to_update.review = movie_form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
 
 
 
+    return render_template("update.html", movie=movie_to_update, fform=movie_form)
+
+
+@app.route('/delete')
+def delete():
+    mv_id = request.args.get('id')
+    the_movie = db.get_or_404(Movie, mv_id)
+    db.session.delete(the_movie)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 
